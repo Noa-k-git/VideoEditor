1c8e1e7d48cbc5b7ec12dd42de718ce97556227d
Noa-k-git <104233360+Noa-k-git@users.noreply.github.com>
2022-10-28 22:56:55 +0300

    bezier base - finished + Added Point structure

--- VideoEditor/Bezier.cpp
+++ VideoEditor/Bezier.cpp
@@ -1,28 +1,106 @@
 #include "Bezier.h"
 
-Bezier::Bezier()
+inline Point::Point() {
+	this->x = 0;
+	this->y = 0;
+}
+
+inline Point::Point(float x, float y)
 {
-	curve.fill({ .5f,.5f });
+	this->x = x;
+	this->y = y;
+}
+
+
+inline Point::Point(std::array<float, 2> point) {
+	this->x = point[0];
+	this->y = point[1];
+}
+
+inline Bezier::Bezier()
+{
+	curve.push_back({ 0,0 });
+	curve.push_back({ 1,1 });
 };
 
-Bezier::Bezier(std::array<std::array<float, 2>, 2> newCurve) : Bezier::Bezier()
+inline Bezier::Bezier(std::vector<Point> newCurve, bool insert) : Bezier::Bezier()
 {
-	SetCurve(newCurve);
+	if (insert)
+		SetCurve(newCurve, 1);
+	else
+		SetCurve(newCurve);
 };
 
-void Bezier::SetCurve(std::array<std::array<float, 2>, 2> newCurve)
+inline void Bezier::SetCurve(std::vector<Point> newCurve)
 {
 	if (CurveValuesCheck(newCurve))
-		this->curve = newCurve;
+			this->curve = newCurve;
 };
 
-float Bezier::cubicCurve(float t)
+inline void Bezier::SetCurve(std::vector<Point> values, int insert)
 {
-	float P0 = this->curve[0][0];
-	return pow((1 - t), 3) * P0 + 3 * pow((1 - t), 2) * t * P1 + 3 * (1 - t) * pow(t, 2) * P2 + pow(t, 3) * P3;
+	if (CurveValuesCheck(values))
+		this->curve.insert(curve.begin() + insert, values.begin(), values.end());
 };
 
-inline bool Bezier::CurveValuesCheck(std::array<std::array<float, 2>, 2>&)
+inline void Bezier::CurveValue(float t, Point& pFinal)
+{
+	switch (curve.size()) {
+	case 2:
+		pFinal = Point(t, t);
+		break;
+	case 3:
+		QuadraticCurve(t, pFinal);
+		break;
+	case 4:
+		CubicCurve(t, pFinal);
+		break;
+	default:
+		break;
+	}
+
+};
+
+inline void Bezier::QuadraticCurve(float t, Point& pFinal)
+{
+	float s = 1 - t;
+	// By the formula
+	pFinal.x = pow(s, 2) * curve[0].x +
+		s * 2 * t * curve[1].x +
+		t * t * curve[2].x;
+	pFinal.y = pow(s, 2) * curve[0].y +
+		s * 2 * t * curve[1].y +
+		t * t * curve[2].y;
+}
+
+inline void Bezier::CubicCurve(float t, Point& pFinal)
+{
+	float s = 1 - t;
+	// By Bezier curve formula
+	pFinal.x = pow(s, 3) * curve[0].x + 
+		3 * pow(s, 2) * t * curve[1].x + 
+		3 * s * pow(t, 2) * curve[2].x + 
+		pow(t, 3) * curve[3].x;
+
+	pFinal.x = pow((1 - t), 3) * curve[0].y +
+		3 * pow((1 - t), 2) * t * curve[1].y + 
+		3 * (1 - t) * pow(t, 2) * curve[2].y + 
+		pow(t, 3) * curve[3].y;
+}
+/*
+void Bezier::GenericCurve(float, Point&)
+{
+	Point p0, p1, mid;
+	std::vector<Point> ;
+	for (int i = 1; i < curve.size() - 2; i++) {
+		p0 = curve[i];
+		p1 = curve[i + 1];
+		mid.x = (p0.x + p1.x) / 2;
+		mid.y = (p0.y + p1.y) / 2;
+	}
+};*/
+
+inline bool Bezier::CurveValuesCheck(std::vector<Point>&)
 {
 	return true;
 };
--- VideoEditor/Bezier.h
+++ VideoEditor/Bezier.h
@@ -1,21 +1,33 @@
-#include <map>
-#include <string>
+#pragma once
 #include <array>
+#include <vector>
 #include <cmath>
 
-using std::map;
-using std::string;
-#pragma once
+struct Point {
+	float x;
+	float y;
+
+	inline Point();
+	inline Point(float x, float y);
+	Point(std::array<float, 2>);
+	~Point() {};
+};
+
 class Bezier
 {
 private:
-	std::array<std::array<float, 2>, 2> curve;
+	std::vector<Point> curve; // Points for bezier curve calculation
+
 public:
-	Bezier();
-	Bezier(std::array<std::array<float, 2>, 2>);
-	void SetCurve(std::array<std::array<float, 2>, 2> curve);
-	float static cubicCurve(float);
+	inline Bezier();
+	inline Bezier(std::vector<Point>, bool);
+	inline void SetCurve(std::vector<Point>);
+	inline void SetCurve(std::vector<Point>, int insert);
+	inline void CurveValue(float t, Point& pFinal);
 private:
-	inline bool CurveValuesCheck(std::array<std::array<float, 2>, 2>&);
+	inline void QuadraticCurve(float, Point&);
+	inline void CubicCurve(float, Point&);
+	//void GenericCurve(float, Point&);
+	inline bool CurveValuesCheck(std::vector<Point>&);
 };
 
